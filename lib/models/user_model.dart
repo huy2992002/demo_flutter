f543317447748d4912990f3f075ee8bc935575af
huy2992002 <huy2992018@gmail.com>
2023-12-19 10:13:29 +0700

    update user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,7 +1,10 @@
+import 'package:demo_flutter/models/user_detail_model.dart';
+
 class UserModel {
   String? avatar;
   String? name;
   bool? role;
+  UserDetailModel? detail;
 
   UserModel();
 }
@@ -9,4 +12,13 @@ class UserModel {
 UserModel user1 = UserModel()
   ..avatar = 'assets/images/img_user_1.png'
   ..name = 'Trần Văn Trung'
-  ..role = false;
+  ..role = false
+  ..detail = (UserDetailModel()
+    ..position = 'CEO/ Founder'
+    ..company = 'Công ty Cổ phần Giải pháp Công nghệ HifivePLUS'
+    ..phone = '0905 550 123'
+    ..phoneCompany = '0905 550 123'
+    ..email = 'trantran@gmail.com'
+    ..website = 'hifiveplus.com.vn'
+    ..facebook = 'Trần Văn Trung'
+    ..zalo = '0905 550 123');
